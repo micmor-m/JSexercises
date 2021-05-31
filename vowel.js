700fceb72592a95b2216b087977e65604315e2a3
mic <micmor.m@gmail.com>
2021-05-30 22:21:43 -0400

    add vowel with regular expression

--- vowel.js
+++ vowel.js
@@ -9,6 +9,17 @@ function vowels(str) {
   return counter;
 }
 
+// With reg expression
+function vowelsRegExpr(str) {
+  const matches = str.match(/[aeiou]/gi); // g is for dont stop at the first match but keep looking in all string
+  // i is key insensitive to compare capital and small letters
+  return matches ? matches.length : 0;
+}
+
 console.log(vowels("Hi there!"));
 console.log(vowels("WHY DO YOU ASK?"));
 console.log(vowels("why"));
+
+console.log(vowelsRegExpr("Hi there!"));
+console.log(vowelsRegExpr("WHY DO YOU ASK?"));
+console.log(vowelsRegExpr("why"));
